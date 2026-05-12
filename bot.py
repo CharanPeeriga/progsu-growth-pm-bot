ea1c420befd5112ddb268e97d905439985bd6de6
CharanPeeriga <charan.peeriga@gmail.com>
2026-05-12 16:23:21 -0400

    sync issues fix

--- bot.py
+++ bot.py
@@ -50,6 +50,7 @@ async def on_ready():
 @bot.command()
 @commands.is_owner()
 async def sync(ctx):
+    bot.tree.clear_commands(guild=ctx.guild)
     bot.tree.copy_global_to(guild=ctx.guild)
     synced = await bot.tree.sync(guild=ctx.guild)
     await ctx.send(f"✅ Synced {len(synced)} command(s) to this server instantly.")
